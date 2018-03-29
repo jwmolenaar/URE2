d77163264a1f485640fbaad2778201c59260170a
S0016608058 <jan.willem.molenaar@mccoy-partners.com>
2018-03-29 10:51:25 +0000

    Initial commit

---
+++ webapp/controller/Startpage.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"jquery.sap.global",
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(jQuery, Controller, JSONModel) {
+	"use strict";
+
+	return Controller.extend("mccoy.com.URE2.controller.Startpage", {
+		onInit: function() {
+
+		}
+	});
+});
