e080398c4a921753ae13f0f22b453736d20d7e09
darmeshnana <darmesh.nana@mccoy-partners.com>
2018-05-25 08:00:37 +0000

    Adjustments for New Run

--- webapp/controller/NewRun.controller.js
+++ webapp/controller/NewRun.controller.js
@@ -1,30 +1,60 @@
-sap.ui.define([	
-	"sap/ui/core/mvc/Controller",
-	"sap/ui/core/routing/History" 
-], function(Controller, History) {
+sap.ui.define([	"sap/ui/core/mvc/Controller",
+				"sap/ui/core/routing/History",
+				"sap/ui/model/json/JSONModel" ], function(Controller, History, JSONModel) {
 	"use strict";
-
 
 	return Controller.extend("mccoy.com.URE2.controller.NewRun", {
 
-		onBack : function () {
-			//var oModel = this.getView().getModel();
-			var sPreviousHash = History.getInstance().getPreviousHash();
+		onInit: function() {
+			// Local View Model used to manage data within this view
+			var oViewModel = new JSONModel({
+			});
+			this.getView().setModel(oViewModel, "newRunForm");
+		},
 
+		onBack : function () {
 			//The history contains a previous entry
-			if (sPreviousHash !== undefined) {
-				// window.history.go(-1);
-				this.getOwnerComponent().getRouter().navTo("page1", null, true);
-			}
-			else	// There is no history!				
-				// replace the current hash with page 1 (will not add an history entry)
+			if (History.getInstance().getPreviousHash() !== undefined)
+				window.history.go(-1);
+			else	// There is no history! Replace the current hash with start page (will not add an history entry)
 				this.getOwnerComponent().getRouter().navTo("home", null, true);
 		},
 		
 		onCarTypeRBChange : function() {
-			
+			var newRunModel = this.getView().getModel("newRunForm");
+		},
+		
+		_createNewRun : function(oView) {
+		//*******************************************************************************
+		// Function will transfer data collected into the local view model towards the backend
+		//*******************************************************************************
+			// increment raceID
+			// fire backend odata service as was done in Startpage (spath)
+
+			//Get number of runs.
+			var sPath = "/URE_METADATA/$count";
+			var oUreMetadata = this.getOwnerComponent().getModel("UreMetadata");
+			oUreMetadata.read(sPath, {
+				success: function(oData, response) {
+					var newRunID = parseInt(oData);
+					newRunID++;
+				},
+				error: function(oError) {
+					// var oError = oError;
+				}
+			});
+
+		},
+		
+		onCreatePress : function() {
+			var newRunModel = this.getView().getModel("newRunForm");
+			// Gather data from local model and trigger backend registration
+			this._createNewRun(this.getView());
 		}
 
+
 	});
+	
+
 
 });
