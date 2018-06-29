ebf1538a8335ef5a9bd6af888ee8154b8e42278d
darmeshnana <darmesh.nana@mccoy-partners.com>
2018-06-29 11:08:35 +0000

    Stop button updates backend

--- webapp/controller/Dashboard.controller.js
+++ webapp/controller/Dashboard.controller.js
@@ -93,36 +93,50 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 			//*******************************************************************************
 			// Stop the run by updating the end timestamp and go back to the home view
 			//*******************************************************************************
-/*			var oView = this.getView();
+			var oView = this.getView();
 			var oUreMetadata = this.getOwnerComponent().getModel("UreMetadata");
-			var oFormData = this.getView().getModel();
+			// var oFormData = this.getView().getModel();
 			var oRouter = this.getOwnerComponent().getRouter();
 
 			var bCompact = !!oView.$().closest(".sapUiSizeCompact").length;
 
-			var raceID = 300;
+			var oViewModel = this.getView().getModel("viewModel");
+
+			// var raceID = oViewModel.raceID;
+			var raceID = oViewModel.getProperty("/raceID");
 			var runID = 1;
-			var oRaceMetaData = sap.ui.getCore().getModel("oRaceMetaData");
 			var oPath = "/URE_METADATA(RACE_ID=" + raceID + ",RUN_ID=" + runID + ")";
-			oRaceMetaData.setProperty(oPath + "/END_TIME", new Date());
+			var oData = {
+				    RACE_ID: raceID,
+				    RUN_ID: 1,
+				    END_TIME: ""
+				};
+			oData.END_TIME = new Date();
 
-			if (oRaceMetaData.hasPendingChanges()) {
-				oRaceMetaData.submitChanges({
-					success: function(oData) {
-						sap.m.MessageToast.show("Test opgeslagen");
-						oRouter.navTo("home", null, true);
-					},
-					error: function(oError) {
-						sap.m.MessageToast.show("Error with saving current test");
-					}
-				});
-			} else
-				sap.m.MessageToast.show("No pending changes current test")*/
+			oUreMetadata.update(oPath, oData, 
+						{	success: function() { oRouter.navTo("home", null, true); },
+							error: function(oResponse) {
+									MessageBox.confirm(
+										oResponse.responseText,
+										{	actions: [sap.m.MessageBox.Action.OK, sap.m.MessageBox.Action.CANCEL],
+											styleClass: bCompact ? "sapUiSizeCompact" : "",
+											onClose: function(sAction) {
+												sap.ui.core.UIComponent.getRouterFor(oView).navTo("page1", null, true);
+											}
+										}
+									);
 
-		},
 
-		// oUreMetadata.setProperty("/END_TIME", new Date());
-		// oUreMetadata.update("/URE_METADATA", {
+								// MessageBox.error( "error", {
+								// 		actions: [sap.m.MessageBox.Action.OK, sap.m.MessageBox.Action.CANCEL],
+								// 		styleClass: bCompact ? "sapUiSizeCompact" : "",
+								// 		onClose: function(sAction) { oRouter.navTo("home", null, true); }
+								// 	}
+								// );
+						}
+			});
+
+		// 	oUreMetadata.update(oPath, oData,
 		// 	success: function() { // Go to the Dashboard
 		// 		oRouter.navTo("home", null, true);
 		// 	},
@@ -135,6 +149,12 @@ sap.ui.define(["sap/ui/core/mvc/Controller",
 		// 			}
 		// 		);
 		// 	}
+		// );		
+			
+		},
+
+		// oUreMetadata.setProperty("/END_TIME", new Date());
+		// oUreMetadata.update("/URE_METADATA", {
 		// });
 
 		_onRouteMatchedDashboard: function(oEvent) {
